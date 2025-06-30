85d122377539f7f17eaa4b0afdda41c24ce6c064
ca46 <carolj8301@gmail.com>
2025-06-30 18:50:02 +0530

    LOGOUT CHANGED

--- Sevenmart/src/main/java/pages/LogoutPage.java
+++ Sevenmart/src/main/java/pages/LogoutPage.java
@@ -5,34 +5,32 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LogoutPage {
+public class LogoutPage 
+{
 
-	@FindBy(xpath="//input[@placeholder='Username']")WebElement uname;
-	@FindBy(xpath="//input[@placeholder='Password']")WebElement pword;
-	@FindBy(xpath="//button[@type='submit']")WebElement signin;
-	@FindBy(xpath="//div/nav/ul[2]/li/a")WebElement image;
-	@FindBy(xpath="//div/nav/ul[2]/li/div/a[2]")WebElement logout;
-	@FindBy(xpath="//button[text()='Sign In']")WebElement signinpage;
-	
+	@FindBy(xpath="//input[@placeholder='Username']")WebElement usernameField;
+	@FindBy(xpath="//input[@placeholder='Password']")WebElement passwordField;
+	@FindBy(xpath="//button[@type='submit']")WebElement signInButton;
+	@FindBy(xpath="//div/nav/ul[2]/li/a")WebElement profileImage;
+	@FindBy(xpath="//div/nav/ul[2]/li/div/a[2]")WebElement logoutButton;
+	@FindBy(xpath="//button[text()='Sign In']")WebElement signInPageButton;
+
 	public WebDriver driver;
-	
-	public LogoutPage(WebDriver driver)
-	{
-		this.driver=driver;
+
+	public LogoutPage(WebDriver driver) {
+		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
-	public void admin()
-	{
-		image.click();
+
+	public void openUserDropdown() {
+		profileImage.click();
 	}
-	public void userLogout()
-	{
-		logout.click();
+
+	public void userLogout() {
+		logoutButton.click();
 	}
-	
-	public boolean signinPageDisplayed()
-	{
-		return signinpage.isDisplayed();
+
+	public boolean isSignInPageDisplayed() {
+		return signInPageButton.isDisplayed();
 	}
 }
--- Sevenmart/src/test/java/testscript/LogoutTest.java
+++ Sevenmart/src/test/java/testscript/LogoutTest.java
@@ -9,25 +9,23 @@ import pages.LoginPage;
 import pages.LogoutPage;
 import utilities.ExcelUtility;
 
-public class LogoutTest extends Base{
-    
-	@Test(retryAnalyzer=retry.Retry.class)
-	public void isUserIsAbleToLogOut() throws IOException
-	{
-		String username=ExcelUtility.getStringData(1, 0, "loginpage");
-		String password=ExcelUtility.getStringData(1, 1, "loginpage");
-		
-		LoginPage loginpage=new LoginPage(driver);
-		loginpage.enterTheUserName(username);
-		loginpage.enterThePassword(password);
-		loginpage.clickTheSignInButton();
-		
-		LogoutPage logoutpage=new LogoutPage(driver);
-		logoutpage.admin();
-		logoutpage.userLogout();
-		
-		boolean signpage=logoutpage.signinPageDisplayed();
-		Assert.assertTrue(signpage);
-		
-	}
+public class LogoutTest extends Base {
+
+    @Test(retryAnalyzer = retry.Retry.class)
+    public void verifyUserIsAbleToLogout() throws IOException {
+        String username = ExcelUtility.getStringData(1, 0, "loginpage");
+        String password = ExcelUtility.getStringData(1, 1, "loginpage");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterTheUserName(username);
+        loginPage.enterThePassword(password);
+        loginPage.clickTheSignInButton();
+
+        LogoutPage logoutPage = new LogoutPage(driver);
+        logoutPage.openUserDropdown();   
+        logoutPage.userLogout();
+
+        boolean isSignInDisplayed = logoutPage.isSignInPageDisplayed();
+        Assert.assertTrue(isSignInDisplayed, "Sign-in page not displayed after logout.");
+    }
 }
